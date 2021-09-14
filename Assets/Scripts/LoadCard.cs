cc131daafeb1553c6401fa74b5152a34cd4e000c
R0ma-N <x6wert@gmail.com>
2021-09-14 17:42:38 +0300

    Initial commit

---
+++ Assets/Scripts/LoadCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadCard : MonoBehaviour
+{
+    [SerializeField] GameObject card;
+    [SerializeField] Canvas canvas;
+
+    void Start()
+    {
+        Instantiate(card);
+        card.transform.SetParent(canvas.transform, false);
+        //card.transform.parent = canvas.transform;
+    }
+}
