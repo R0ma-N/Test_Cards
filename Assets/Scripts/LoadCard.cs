f44011d6ca93696f64cb59b7a34ce4ffb115b11c
R0ma-N <x6wert@gmail.com>
2021-09-15 12:04:05 +0300

    add cards

--- Assets/Scripts/LoadCard.cs
+++ Assets/Scripts/LoadCard.cs
@@ -6,12 +6,30 @@ using UnityEngine.UI;
 public class LoadCard : MonoBehaviour
 {
     [SerializeField] GameObject card;
-    [SerializeField] Canvas canvas;
+    Transform[] places;
+    [SerializeField] HorizontalLayoutGroup horizontalLayout;
+    public float spacing;
+    float timeElapsed;
+    [SerializeField] float lerpDuration = 3;
 
     void Start()
     {
-        Instantiate(card);
-        card.transform.SetParent(canvas.transform, false);
-        //card.transform.parent = canvas.transform;
+        places = GetComponentsInChildren<Transform>();
+        spacing = horizontalLayout.spacing;
+
+        for (int i = 1; i < Random.Range(5,7); i++)
+        {
+            GameObject shadowObject = Instantiate(card);
+            shadowObject.transform.SetParent(places[i].transform, false);
+        }
+    }
+
+    private void Update()
+    {
+        if (timeElapsed < lerpDuration)
+        {
+            horizontalLayout.spacing = Mathf.Lerp(0, 74, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+        }
     }
 }
