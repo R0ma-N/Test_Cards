7a207428ba78352e7a749934fcf423eca6d7322e
R0ma-N <x6wert@gmail.com>
2021-09-16 17:25:28 +0300

    Card Values

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Image cardArt;
+    [SerializeField] private Text attackText, healthText, manaText;
+    private int _attackValue, _healthValue, _manaValue;
+
+    private Sprite _image;
+    private string url = "https://picsum.photos/200";
+
+    public int AttackValue
+    {
+        get { return _attackValue; }
+        set 
+        { 
+            _attackValue = value;
+            attackText.text = AttackValue.ToString();
+        }
+    }
+
+    public int Health
+    {
+        get { return _healthValue; }
+        set
+        {
+            _healthValue = value;
+            attackText.text = Health.ToString();
+        }
+    }
+
+    public int Mana
+    {
+        get { return _manaValue; }
+        set
+        {
+            _manaValue = value;
+            attackText.text = Mana.ToString();
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(LoadFromWebCoroutine());
+
+       // parametr = new Text[] { attackText, healthText, manaText };
+        AttackValue = Random.Range(1, 10);
+        _healthValue = Random.Range(1, 10);
+        _manaValue = Random.Range(1, 10);
+
+        healthText.text = _healthValue.ToString();
+        manaText.text = _manaValue.ToString();
+    }
+
+    public void ChangeValue()
+    {
+        // parametr[Random.Range(1, 4)].text = Random.Range(-2, 10).ToString();
+    }
+
+    private IEnumerator LoadFromWebCoroutine()
+    {
+        WWW wwwLoader = new WWW(url);
+        yield return wwwLoader;
+
+        Texture2D loadedTexture = wwwLoader.texture;
+        _image = Sprite.Create(loadedTexture, new Rect(0, 0, loadedTexture.width, loadedTexture.height), new Vector2(0.5f, 0.5f));
+        cardArt.sprite = _image;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ChangeValue();
+        }
+    }
+
+    private void Counter()
+    {
+
+    }
+}
+
--- Assets/Scripts/LoadCard.cs
+++ Assets/Scripts/LoadCard.cs
@@ -7,29 +7,84 @@ public class LoadCard : MonoBehaviour
 {
     [SerializeField] GameObject card;
     Transform[] places;
+    List<Card> cards = new List<Card>();
+
+    [SerializeField] Canvas canvas;
     [SerializeField] HorizontalLayoutGroup horizontalLayout;
     public float spacing;
     float timeElapsed;
     [SerializeField] float lerpDuration = 3;
 
+    Transform parent;
+    int activeCardNumber = 0;
+    int cardsCount;
+
     void Start()
     {
         places = GetComponentsInChildren<Transform>();
-        spacing = horizontalLayout.spacing;
+        cardsCount = Random.Range(4, 7);
 
-        for (int i = 1; i < Random.Range(5,7); i++)
+        for (int i = 1; i < cardsCount + 1; i++)
         {
-            GameObject shadowObject = Instantiate(card);
-            shadowObject.transform.SetParent(places[i].transform, false);
+            GameObject tempObject = Instantiate(card);
+            tempObject.transform.SetParent(places[i].transform, false);
+            cards.Add(tempObject.GetComponent<Card>());
         }
+
+        //parent = cards[0].transform.parent;
+        //cards[0].transform.SetParent(canvas.transform);
     }
 
     private void Update()
     {
-        if (timeElapsed < lerpDuration)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            horizontalLayout.spacing = Mathf.Lerp(0, 74, timeElapsed / lerpDuration);
-            timeElapsed += Time.deltaTime;
+            cards[0].transform.SetParent(parent);
         }
+
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            cards[0].transform.SetParent(canvas.transform);
+        }
+    }
+
+    public void SelectCard(Card card)
+    {
+        parent = card.transform.parent;
+        card.transform.SetParent(canvas.transform);
+    }
+
+    private void DeselectCard(Card card)
+    {
+        card.transform.SetParent(parent);
+    }
+
+    public void NextCard()
+    {
+        print($"cardNumber {activeCardNumber}");
+        print($"cardsCount {cardsCount}");
+
+        if (activeCardNumber == 0)
+        {
+            SelectCard(cards[activeCardNumber]);
+            activeCardNumber++;
+        }
+        else if (activeCardNumber > 0)
+        {
+            DeselectCard(cards[activeCardNumber - 1]);
+            SelectCard(cards[activeCardNumber]);
+
+            if (activeCardNumber < cardsCount-1)
+            {
+                activeCardNumber++;
+            }
+            else
+            {
+                DeselectCard(cards[cardsCount - 1]);
+                activeCardNumber = 0;
+            }
+        }
+
+        cards[activeCardNumber].ChangeValue();
     }
 }
